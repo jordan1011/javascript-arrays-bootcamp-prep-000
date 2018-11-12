8fed8dc0a1637c96b6fb5642a7ba5574bf5fc311
jordancortes22 <jordancortes22@gmail.com>
2018-11-12 22:45:17 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ["snickers", "hunderd grand", "kitkat", "skittles"]
+
+function addElmentToBeginningOfArray (chocolateBars,ferrero){
+  chocolateBars.unshift("ferrero")
+}
+
+function destructivelyAddElementToBeginningOfArray()
